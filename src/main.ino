e945287ea407927021ac3d168509c416ac9c49e3
hiibrarahmad <hiibrarahmad@gmail.com>
2023-05-28 05:38:07 +0500

    build flag added to solve watchdog trigger

--- src/main.ino
+++ src/main.ino
@@ -4,6 +4,9 @@
 #include <TJpg_Decoder.h>
 #include <TFT_eSPI.h>
 #include <ezButton.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
 #include "driver/i2s.h"
 
 const char* ssid = "ibrar";
@@ -16,25 +19,49 @@ WebsocketsClient client;
 
 SemaphoreHandle_t connection_established = NULL;
 SemaphoreHandle_t button_semaphore = NULL;
+SemaphoreHandle_t audio_task_semaphore = NULL;
 TaskHandle_t ws_comm_task = NULL;
 TaskHandle_t button_task = NULL;
+TaskHandle_t audio_task = NULL; 
 
 TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
+
+void ws_comm(void* arg);
+void button_task_func(void* arg);
+void audio(void* arg);
+
 
 #define I2S_WS_TX  12
 #define I2S_SCK_TX 13
 #define I2S_DATA_OUT_TX  15
-
 #define I2S_PORT I2S_NUM_0
 #define I2S_SAMPLE_RATE   (16000)
 #define I2S_SAMPLE_BITS   (32)
 #define UPDATE_INTERVAL   (500)
 
-ezButton button(26); // Pin for the button
 
-bool buttonPressed = false;
-unsigned long lastButtonPressTime = 0;
-const unsigned long buttonCooldownTime = 5000; // Cooldown time in milliseconds
+unsigned long last_update_sent = 0;
+bool isConnected;
+static int sampleValue;
+
+const i2s_config_t i2s_config_tx = {
+  .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_TX),
+  .sample_rate = I2S_SAMPLE_RATE,
+  .bits_per_sample = i2s_bits_per_sample_t(I2S_SAMPLE_BITS),
+  .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+  .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+  .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+  .dma_buf_count = 32,
+  .dma_buf_len = 64
+};
+
+const i2s_pin_config_t pin_config_tx = {
+  .bck_io_num = I2S_SCK_TX, // LRC
+  .ws_io_num = I2S_WS_TX,   //blck
+  .data_out_num = I2S_DATA_OUT_TX, //din
+  .data_in_num = I2S_PIN_NO_CHANGE
+
+};
 
 bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap)
 {
@@ -48,16 +75,16 @@ bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap)
   return 1;
 }
 
-void ws_comm(void* arg);
-void button_task_func(void* arg);
-void i2s_write_from_client();
-void i2sInit();
-static void server_task(void *arg);
+ezButton button(26); // Pin for the button
+
+bool buttonPressed = false;
+unsigned long lastButtonPressTime = 0;
+const unsigned long buttonCooldownTime = 5000; // Cooldown time in milliseconds
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
-
+  i2sInit();
   tft.begin();
   tft.setRotation(3);
   tft.setTextColor(0xFFFF, 0x0000);
@@ -79,22 +106,65 @@ void setup() {
   Serial.println(IP);
 
   server.listen(8888);
+  
 
   button.setDebounceTime(50); // Set debounce time to 50 milliseconds
 
   connection_established = xSemaphoreCreateBinary();
   button_semaphore = xSemaphoreCreateBinary();
-
+  audio_task_semaphore = xSemaphoreCreateBinary();
   xTaskCreatePinnedToCore(ws_comm, "websocket server", 4096, NULL, 5, &ws_comm_task, 1);
   xTaskCreatePinnedToCore(button_task_func, "button task", 2048, NULL, 5, &button_task, 1);
+
+  xTaskCreate(audio, "audio_task", 8192, NULL, 10, &audio_task);
+  
+  //xTaskCreatePinnedToCore(audio, "audio rcv", 4096, NULL, 5, &audio_task, 1);
+
+
+  
 }
 
 void loop() {
-  if (server.poll()) {
+ if (server.poll()) {
     client = server.accept();
-    xSemaphoreGive(connection_established);
+     xSemaphoreGive(connection_established);
+     xSemaphoreGive(audio_task_semaphore);
+     
+ }
+}
+
+
+
+void audio(void* arg) {
+  while (1) {
+    if (xSemaphoreTake(audio_task_semaphore, portMAX_DELAY) == pdTRUE) {
+    while (client.available()){
+    last_update_sent = millis();
+    client.poll();
+    WebsocketsMessage msg = client.readBlocking();
+    char *buf_ptr = (char*)msg.c_str();
+    size_t bytes_written = static_cast<size_t>(msg.length());
+    i2s_write(I2S_PORT, buf_ptr, bytes_written, &bytes_written, portMAX_DELAY);
+
+    int32_t sample = (int32_t)buf_ptr[3] << 24;
+    sample |= (int32_t)buf_ptr[2] << 16;
+    sample |= (int32_t)buf_ptr[1] << 8;
+    sample |= (int32_t)buf_ptr[0];
+    }
+
   }
-  i2s_write_from_client();
+    
+    vTaskDelay(pdMS_TO_TICKS(10)); // Adjust delay as needed
+  }
+}
+
+
+
+
+
+void i2sInit() {
+  i2s_driver_install(I2S_PORT, &i2s_config_tx, 0, NULL);
+  i2s_set_pin(I2S_PORT, &pin_config_tx);
 }
 
 void button_task_func(void* arg) {
@@ -160,47 +230,5 @@ void ws_comm(void* arg) {
         vTaskDelay(pdMS_TO_TICKS(20)); // Adjust delay as needed
       }
     }
-  }
-}
-
-void i2s_write_from_client()
-{
-  if (client.available()) {
-    last_update_sent = millis();
-    client.poll();
-    WebsocketsMessage msg = client.readBlocking();
-    char *buf_ptr = (char*)msg.c_str();
-    size_t bytes_written = static_cast<size_t>(msg.length());
-    i2s_write(I2S_PORT, buf_ptr, bytes_written, &bytes_written, portMAX_DELAY);
-
-    int32_t sample = (int32_t)buf_ptr[3] << 24;
-    sample |= (int32_t)buf_ptr[2] << 16;
-    sample |= (int32_t)buf_ptr[1] << 8;
-    sample |= (int32_t)buf_ptr[0];
-  }
-}
-
-void i2sInit() {
-  i2s_driver_install(I2S_PORT, &i2s_config_tx, 0, NULL);
-  i2s_set_pin(I2S_PORT, &pin_config_tx);
-}
-
-static void server_task(void *arg)
-{
-  while (1) {
-    if (server.poll()) {
-      client = server.accept();
-      i2sInit();
-      last_update_sent = millis();
-      isConnected = true;
-      Serial.println("socket connected");
-    }
-
-    if (isConnected && millis() - last_update_sent > UPDATE_INTERVAL) {
-      Serial.println("Client disconnected!");
-      isConnected = false;
-      i2s_driver_uninstall(I2S_PORT);
-    }
-    vTaskDelay(100);
   }
 }
